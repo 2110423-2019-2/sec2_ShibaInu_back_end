adb1d3a92fc4cf95092cd030b98fee571e45dd30
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2020-02-05 14:05:54 +0700

    Add prettier config, test api

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    getHello(): string {
+        return 'Hello World!';
+    }
+
+    async getPing(): Promise<string> {
+        return 'Pong';
+    }
 }
