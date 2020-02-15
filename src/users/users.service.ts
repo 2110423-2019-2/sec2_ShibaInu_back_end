d893028b1173150365b3e65164ce0c75589891a5
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2020-02-15 12:20:40 +0700

    Change to use hashed password

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './users.dto';
+import bcrypt = require('bcrypt');
 
 @Injectable()
 export class UsersService {
@@ -20,6 +21,9 @@ export class UsersService {
     }
 
     async createNewUser(createUserDto: CreateUserDto) {
+        const hashedPass = await bcrypt.hash(createUserDto.password, 10);
+        createUserDto.password = hashedPass;
+
         createUserDto.createdTime = new Date();
         createUserDto.isVerified = false;
         createUserDto.isVisible = true;
