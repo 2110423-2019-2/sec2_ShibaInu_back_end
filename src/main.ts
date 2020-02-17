c584501ea9b4aecf97183dac486e81c4d8a4a11f
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2020-02-17 23:57:26 +0700

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.enableCors();
     await app.listen(10000);
 }
 bootstrap();
