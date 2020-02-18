6fc9c302dfa7718f486ded97045a65a7f0cc3799
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2020-02-18 21:17:38 +0700

    fix(auth): add PassportModule import, also some other refactors

--- src/jobs/jobs.module.ts
+++ src/jobs/jobs.module.ts
@@ -3,9 +3,10 @@ import { JobsController } from './jobs.controller';
 import { JobsService } from './jobs.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Job } from '../entities/job.entity';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Job])],
+    imports: [TypeOrmModule.forFeature([Job]), PassportModule.register({ defaultStrategy: 'jwt' })],
     controllers: [JobsController],
     providers: [JobsService],
 })
