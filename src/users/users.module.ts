ac00a5a7e2a3b7858f69130371c5386154c8a984
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2020-02-18 21:17:38 +0700

    fix(auth): add PassportModule import, also some other refactors

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User, InterestedCategory } from '../entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([User]),TypeOrmModule.forFeature([InterestedCategory])],
+	imports: [TypeOrmModule.forFeature([User, InterestedCategory]),PassportModule.register({ defaultStrategy: 'jwt' })],
 	controllers: [UsersController],
 	providers: [UsersService],
 	exports: [UsersService],
