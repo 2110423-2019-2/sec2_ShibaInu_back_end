f6557e047eb85810f548ee6c1e650d87a9ae1a67
Pseudoku12 <purinatth@gmail.com>
2020-02-19 16:49:23 +0700

    Add jobId and userId to bids.dto.ts

--- src/bids/bids.dto.ts
+++ src/bids/bids.dto.ts
@@ -3,6 +3,8 @@ import { Job } from '../entities/job.entity';
 import { Timestamp } from 'typeorm';
 
 export class CreateBidDto {
+    jobId: number;
+    userId: number;
     biddedWage: number;
     biddedDuration: number;
     createdTime?: Date;
