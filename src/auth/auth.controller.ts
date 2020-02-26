a445dd3c9907218dbce61d0dcf7691c946be0592
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2020-02-26 14:05:47 +0700

    feat(jwtuser): Add decorator to get user data from JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,7 +8,7 @@ export class AuthController {
 
     @UseGuards(AuthGuard('local'))
     @Post('login')
-    async login(@Request() req) {
+    async login(@Request() req: any) {
         return this.authService.login(req.user);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -14,17 +14,17 @@ export class AuthService {
     async validateUser(username: string, password: string) {
         const user = await this.userService.getUserByUsername(username);
         if (!user) {
-            throw new UnauthorizedException(`Invalid username or password`);
+            throw new UnauthorizedException(`Invalid username`);
         }
         if (await bcrypt.compare(password, user.password)) {
+            user.username = username;
             return user;
         }
         return null;
     }
 
-    @UseGuards(AuthGuard('local'))
     async login(user: any) {
-        const payload = { sub: user.id, username: user.username };
+        const payload = { sub: user.userId, username: user.username };
         return { access_token: this.jwtService.sign(payload) };
     }
 }
