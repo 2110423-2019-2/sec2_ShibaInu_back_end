2af1ba5e53231c821de2b265baa21eae9fd3dfe7
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2020-03-11 14:40:41 +0700

    feat(announcement): add guard for admin, attach to APIs

---
+++ src/guards/admin.guard.ts
@@ -0,0 +1,26 @@
+import { CanActivate, ExecutionContext, Injectable, ForbiddenException } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
+import { UsersService } from '../users/users.service';
+
+@Injectable()
+export class AdminGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector,
+    private readonly userService: UsersService) {}
+
+  async canActivate(
+    context: ExecutionContext,
+  ): Promise<boolean> {
+    const isAdmin = this.reflector.get<boolean[]>('isadmin', context.getHandler());
+    if (!isAdmin) {
+      return true;
+    }
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    const userEntity = await this.userService.getUserById(user.id)
+    if(!userEntity.isAdmin) {
+      throw new ForbiddenException(`This feature is for admin only!`)
+    }
+    return userEntity.isAdmin;
+  }
+}
