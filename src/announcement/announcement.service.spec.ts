d47d6e9a81a730ab280f5ed14db0ca9459f17cb0
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2020-03-11 13:35:06 +0700

    feat(announcement): add tests

--- src/announcement/announcement.service.spec.ts
+++ src/announcement/announcement.service.spec.ts
@@ -1,18 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AnnouncementService } from './announcement.service';
+import { Repository } from 'typeorm';
 
 describe('AnnouncementService', () => {
   let service: AnnouncementService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AnnouncementService],
+      providers: [AnnouncementService,
+      {
+        provide: 'AnnouncementRepository',
+        useClass: Repository,
+      }],
     }).compile();
 
     service = module.get<AnnouncementService>(AnnouncementService);
+
+    
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should create an announcement', () => {
+    expect(service.createAnnouncement).toBeCalled()
+  })
 });
