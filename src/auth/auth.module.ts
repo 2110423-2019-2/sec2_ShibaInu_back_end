cf2e9f8a0b799802acb2e01e251f08c34e84b47c
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2020-03-31 17:09:43 +0700

    feat(fblogin): (WIP) add draft code for Facebook login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { LocalStrategy } from './local.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { FacebookStrategy } from './facebook.strategy';
 
 @Module({
     imports: [
@@ -16,6 +17,6 @@ import { JwtStrategy } from './jwt.strategy';
         }),
     ],
     controllers: [AuthController],
-    providers: [AuthService, LocalStrategy, JwtStrategy],
+    providers: [AuthService, LocalStrategy, JwtStrategy, FacebookStrategy],
 })
 export class AuthModule {}
