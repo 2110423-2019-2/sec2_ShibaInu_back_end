9d171d6b6232fdfbacde0913104906c122a5efc4
Kanokphat Jinanarong <kanokphatji@hotmail.com>
2020-04-24 01:45:18 +0700

    swagger: attach swagger to backend

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 require('dotenv').config();
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { cors: true });
+
+    const options = new DocumentBuilder()
+        .addBearerAuth()
+        .setTitle('Youngstar API docs')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api-list', app, document);
+
     await app.listen(10000);
 }
 bootstrap();
